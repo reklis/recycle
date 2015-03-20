b9243a87368095041b5c22423a403467ee820577
sfusco <sfusco@ricin.local>
2015-03-20 00:02:04 -0400

    initial checkin

--- recycle/main.swift
+++ recycle/main.swift
@@ -2,11 +2,27 @@
 //  main.swift
 //  recycle
 //
-//  Created by Steven Fusco on 3/19/15.
-//  Copyright (c) 2015 Steven Fusco. All rights reserved.
+//  usage: recycle <files or directories to throw out>
 //
 
 import Foundation
+import AppKit
 
-println("Hello, World!")
+var args = NSProcessInfo.processInfo().arguments
+args.removeAtIndex(0)   // first item in list is the program itself
 
+var w = NSWorkspace.sharedWorkspace()
+var fm = NSFileManager.defaultManager()
+
+for arg in args {
+    let path = arg.stringByStandardizingPath;
+    
+    let file = path.lastPathComponent
+    let source = path.stringByDeletingLastPathComponent
+    
+    w.performFileOperation(NSWorkspaceRecycleOperation,
+        source:source,
+        destination: "",
+        files: [file],
+        tag: nil)
+}
