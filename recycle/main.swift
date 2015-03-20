14e48585a491ed367ac55466afa8fee43a98dcd0
sfusco <sfusco@ricin.local>
2015-03-19 21:43:13 -0400

    Initial Commit

---
+++ recycle/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  recycle
+//
+//  Created by Steven Fusco on 3/19/15.
+//  Copyright (c) 2015 Steven Fusco. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
